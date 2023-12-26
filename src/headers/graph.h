ce5411d77a0fc152d451ea5a34aafbfb21048e97
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-12-26 16:42:13 +0000

    Update graph.h

--- src/headers/graph.h
+++ src/headers/graph.h
@@ -65,12 +65,6 @@ public:
 template <class T, class W>
 class Graph {
     unordered_map<std::string, Vertex<T, W>*> vertexSet;
-    int _index_;
-    stack<Vertex<T, W>> _stack_;
-    list<list<T>> _list_sccs_;
-
-    void dfsVisit(Vertex<T, W> *v,  vector<T> & res) const;
-    bool dfsIsDAG(Vertex<T, W> *v) const;
 public:
     Vertex<T, W> *findVertex(std::string id);
     int getNumVertex() const;
@@ -79,10 +73,6 @@ public:
     bool addEdge(const T &sourc, const T &dest, double w, W &info);
     bool removeEdge(const T &sourc, const T &dest);
     unordered_map<std::string, Vertex<T, W> *> getVertexSet() const;
-    vector<T> dfs() const;
-    vector<T> dfs(const T & source) const;
-    vector<T> bfs(const T &source) const;
-    bool isDAG() const;
 };
 
 template <class T, class W>
@@ -230,7 +220,11 @@ unordered_map<std::string, Vertex<T, W>*> Graph<T, W>::getVertexSet() const {
  */
 template<class T, class W>
 Vertex<T, W> * Graph<T, W>::findVertex(std::string id) {
-    return vertexSet[id];
+    try {
+		return vertexSet.at(id);
+	} catch (const exception &e) {
+		return NULL;
+	}
 }
 
 /*
@@ -283,145 +277,15 @@ bool Graph<T, W>::removeEdge(const T &sourc, const T &dest) {
 template<class T, class W>
 bool Graph<T, W>::removeVertex(const T &in) {
     for (auto it = vertexSet.begin(); it != vertexSet.end(); it++)
-        if ((*it)->info  == in) {
-            auto v = *it;
+        if (it->second->info == in) {
+            auto v = it->second;
             vertexSet.erase(it);
             for (auto u : vertexSet)
-                u->removeEdgeTo(v);
+                u.second->removeEdgeTo(v);
             delete v;
             return true;
         }
     return false;
-}
-
-
-/****************** DFS ********************/
-/*
- * Performs a depth-first search (dfs) traversal in a graph (this).
- * Returns a vector with the contents of the vertices by dfs order.
- * Follows the algorithm described in theoretical classes.
- */
-template<class T, class W>
-vector<T> Graph<T, W>::dfs() const {
-    vector<T> res;
-    for (auto v : vertexSet)
-        v->visited = false;
-    for (auto v : vertexSet)
-        if (! v->visited)
-            dfsVisit(v, res);
-    return res;
-}
-
-/*
- * Auxiliary function that visits a vertex (v) and its adjacent, recursively.
- * Updates a parameter with the list of visited node contents.
- */
-template<class T, class W>
-void Graph<T, W>::dfsVisit(Vertex<T, W> *v, vector<T> & res) const {
-    v->visited = true;
-    res.push_back(v->info);
-    for (auto & e : v->adj) {
-        auto w = e.dest;
-        if ( ! w->visited)
-            dfsVisit(w, res);
-    }
-}
-
-
-/****************** DFS ********************/
-/*
- * Performs a depth-first search (dfs) in a graph (this).
- * Returns a vector with the contents of the vertices by dfs order,
- * from the source node.
- */
-template<class T, class W>
-vector<T> Graph<T, W>::dfs(const T & source) const {
-    vector<T> res;
-    auto s = findVertex(source);
-    if (s == nullptr)
-        return res;
-
-    for (auto v : vertexSet)
-        v->visited = false;
-
-    dfsVisit(s, res);
-    return res;
-}
-
-
-/****************** BFS ********************/
-/*
- * Performs a breadth-first search (bfs) in a graph (this), starting
- * from the vertex with the given source contents (source).
- * Returns a vector with the contents of the vertices by bfs order.
- */
-template<class T, class W>
-vector<T> Graph<T, W>::bfs(const T & source) const {
-    vector<T> res;
-    auto s = findVertex(source);
-    if (s == NULL)
-        return res;
-    queue<Vertex<T, W> *> q;
-    for (auto v : vertexSet)
-        v->visited = false;
-    q.push(s);
-    s->visited = true;
-    while (!q.empty()) {
-        auto v = q.front();
-        q.pop();
-        res.push_back(v->info);
-        for (auto & e : v->adj) {
-            auto w = e.dest;
-            if ( ! w->visited ) {
-                q.push(w);
-                w->visited = true;
-            }
-        }
-    }
-    return res;
-}
-
-
-/****************** isDAG  ********************/
-/*
- * Performs a depth-first search in a graph (this), to determine if the graph
- * is acyclic (acyclic directed graph or DAG).
- * During the search, a cycle is found if an edge connects to a vertex
- * that is being processed in the stack of recursive calls (see theoretical classes).
- * Returns true if the graph is acyclic, and false otherwise.
- */
-
-template<class T, class W>
-bool Graph<T, W>::isDAG() const {
-    for (auto v : vertexSet) {
-        v->visited = false;
-        v->processing = false;
-    }
-    for (auto v : vertexSet)
-        if (! v->visited)
-            if ( ! dfsIsDAG(v) )
-                return false;
-    return true;
-}
-
-/**
- * Auxiliary function that visits a vertex (v) and its adjacent, recursively.
- * Returns false (not acyclic) if an edge to a vertex in the stack is found.
- */
-template<class T, class W>
-bool Graph<T, W>::dfsIsDAG(Vertex<T, W> *v) const {
-    v->visited = true;
-    v->processing = true;
-    for (auto & e : v->adj) {
-        auto w = e.dest;
-        if (w->processing)
-            return false;
-        if (! w->visited)
-            if (! dfsIsDAG(w))
-                return false;
-    }
-    v->processing = false;
-    return true;
 }
 
 #endif /* GRAPH_H_ */
