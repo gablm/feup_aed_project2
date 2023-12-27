4ef206c46fc1fa5f8f56e4e595d898bb5a56de74
diogocp27 <diogocp27@hotmail.com>
2023-12-27 00:22:06 +0000

    added city airport map

--- src/manager.cpp
+++ src/manager.cpp
@@ -56,7 +56,10 @@ void Manager::loadAirports() {
 		Airport newData = Airport(code, name, city, country, latitude, longitude);
 
 		connections.addVertex(newData, code);
-		available_flights.addVertex(newData, code);
+		if (available_flights.addVertex(newData, code)){
+			Vertex<Airport, Airline> *vertexPointer = available_flights.findVertex(code);
+        	cityAirportList[city].push_back(vertexPointer);
+		}
 		airports[code] = newData;
 	}
 
@@ -81,7 +84,10 @@ void Manager::testAirports() {
 		std::cout 	<< "Search for " + codes + " returns: \n\n" 
 					<< res.getCode() << " - " << res.getCity() 
 					<< "\nLA: " << res.getLatitude() << " LO: " << res.getLongitude() << "\n\n";
-	}	
+	}
+
+
+
 }
 
 void Manager::loadAirlines() {
